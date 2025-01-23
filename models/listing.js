53b7c4661d7ec3667d406785ea774bb574890cd7
ShahidMetkari <metkarishahid@gmail.com>
2025-01-24 00:34:42 +0530

    commit

--- models/listing.js
+++ models/listing.js
@@ -13,7 +13,8 @@ const listingSchema = new Schema({
       "https://images.unsplash.com/photo-1505159940484-eb2b9f2588e2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     //setting up default image
     set: (v) => {
-      return v === ""
+        return v === "" 
+      v === ""
         ? "https://images.unsplash.com/photo-1505159940484-eb2b9f2588e2?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         : v;
     },
